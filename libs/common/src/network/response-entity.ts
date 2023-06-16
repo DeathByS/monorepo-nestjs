e210806a9bf383e5bbfbf45731553671b9494f41
kilho.cho <ghwhrlf@gmail.com>
2023-06-16 21:58:45 +0900

    feat: swagger response decorator, api response entity

---
+++ libs/common/src/network/response-entity.ts
@@ -0,0 +1,47 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class Response {
+  @ApiProperty({ example: 0 })
+  private readonly code: number;
+  @ApiProperty({ example: {} })
+  private readonly data?: any | any[];
+  @ApiProperty({ example: '' })
+  private readonly message?: string;
+
+  constructor(code: number, data: any | any[], message: string) {
+    this.code = code;
+    this.data = data;
+    this.message = message;
+  }
+}
+
+export class ResponseEntity<T> {
+  @ApiProperty({ example: 0 })
+  private code: number;
+  @ApiProperty()
+  private data: T | T[];
+  @ApiProperty({ example: '' })
+  private message?: string;
+  @ApiProperty({ description: 'pagination meta data'})
+  private meta?: any;
+  public ok(): ResponseEntity<T> {
+    this.code = 0;
+    return this;
+  }
+
+  public error(code = 999, message = 'Error'): ResponseEntity<T> {
+    this.code = code;
+    this.message = message;
+    return this;
+  }
+
+  public body(data: T | T[]): ResponseEntity<T> {
+    this.data = data;
+    return this;
+  }
+
+  public setPageMeta(pageMeta: any): ResponseEntity<T> {
+    this.meta = pageMeta;
+    return this;
+  }
+}
